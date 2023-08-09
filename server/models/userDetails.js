0d936adae21ae25a80804ca30c99e2a63fe82600
Bhavyam Mahajan <84068394+BhavyamMahajan@users.noreply.github.com>
2023-08-09 18:58:55 +0530

    Create userDetails.js

---
+++ server/models/userDetails.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const user = mongoose.Schema({
+  username: String,
+  name: String,
+  bio: String,
+  profileImg: String,
+  posts: [String],
+});
+
+module.exports = mongoose.model("userDetails", user);
